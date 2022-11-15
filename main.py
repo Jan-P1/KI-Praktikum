e9d6d6132baebb1a29dca2edff44215fd867ad30
Julian-Heidenreich <julian-heidenreich@users.noreply.github.com>
2022-11-15 10:02:13 +0100

    + Client with random iitialization

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 import xml.etree.ElementTree as ET
+import random
 import time
 import math
 longestNum = 0
 
+class Edge:
+    def __init__(self, dest, value) -> None:
+        self.dest = dest
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Vertex:
+    def __init__(self, id, edges: list[Edge]) -> None:
+        self.id = id
+        self.edges = edges
+
+    def __str__(self):
+        global longestNum
+        res = ""
+        iteration = 0
+        for i in range(len(self.edges) + 1):
+            if self.id == i:
+                res += "X\t\t"
+            else:
+                length = len(f"{str(self.edges[iteration])}")
+                res += f"{str(self.edges[iteration])}"
+                res += "\t" * (math.floor(longestNum / 4) - math.floor(length / 4) + 1)
+                iteration += 1
+
+        return res + "\n"
 
 class Graph:
-    def __init__(self, vertices, name, source, description) -> None:
+    def __init__(self, vertices: list[Vertex], name, source, description) -> None:
         self.vertices = vertices
         self.name = name
         self.source = source
@@ -25,36 +53,6 @@ class Graph:
             strVertex += f"V{i}:\t{str(value)}"
 
         return res + "\n" + strVertex
-
-
-class Vertex:
-    def __init__(self, id, edges) -> None:
-        self.id = id
-        self.edges = edges
-
-    def __str__(self):
-        global longestNum
-        res = ""
-        iteration = 0
-        for i in range(len(self.edges) + 1):
-            if self.id == i:
-                res += "X\t\t"
-            else:
-                length = len(f"{str(self.edges[iteration])}")
-                res += f"{str(self.edges[iteration])}"
-                res += "\t" * (math.floor(longestNum / 4) - math.floor(length / 4) + 1)
-                iteration += 1
-
-        return res + "\n"
-
-
-class Edge:
-    def __init__(self, dest, value) -> None:
-        self.dest = dest
-        self.value = value
-
-    def __str__(self):
-        return str(self.value)
 
 
 def initXML(filename) -> Graph:
@@ -79,7 +77,7 @@ def initXML(filename) -> Graph:
             # TODO bs check depends on construction of xml data and should be changed but not sure how yet
             length = len(str(float(edge.attrib.get('cost'))))
             if float(edge.attrib.get('cost')) < 9990.0:
-                vertEdges.append(Edge(edge.text, float(edge.attrib.get('cost'))))
+                vertEdges.append(Edge(int(edge.text), float(edge.attrib.get('cost'))))
             if length > longestNum:
                 longestNum = length
 
@@ -97,10 +95,84 @@ def initXML(filename) -> Graph:
     return resGraph
 
 
+class Client:
+    def __init__(self, graph: Graph, stops = []) -> None:
+        self.graph = graph
+        self.stops = []
+        self.weights = []
+        if len(stops) == 0:
+            self.initialize_random()
+        else:
+            self.calculate_weight()
+    
+    def __str__(self) -> str:
+        res = "########################################################\nStops in Graph\n"
+        for i in self.stops:
+            res += f"{i}\t"
+        res += "\nWeight in between\n"
+
+        for i in self.weights:
+            res += f"{i}\t"
+        res += "\n"
+
+        return res
+
+    def initialize_random(self):
+        for _ in self.graph.vertices:
+            is_duplicate = True
+            while(is_duplicate):
+                new_random_vertex = random.randint(0, len(self.graph.vertices) - 1)
+                if new_random_vertex not in self.stops:
+                    self.stops.append(new_random_vertex)
+                    is_duplicate = False
+        self.stops.append(self.stops[0])
+        self.calculate_weight()
+
+    def calculate_weight(self):
+        self.weights.clear()
+        # Loop all stops to get new weight
+        for i in range(len(self.stops)):
+            # Prevent list out of bound while looping stops list and trying to access next stop
+            if not (len(self.weights) > 1 and self.stops[i] is self.stops[0]):
+                # Loop all vertex edges and find the correct next one and add the wight in between
+                source = self.graph.vertices[self.stops[i]]
+                for e in source.edges:
+                    if self.stops[i + 1] is e.dest:
+                        self.weights.append(e.value)
+                        break
+
+    def completed(self) -> bool:
+        if len(self.graph) == len(self.stops):
+            return True
+        else:
+            return False
+
+    def completed_weight(self) -> int:
+        return sum(self.weights)
+
+    def mutation(self):
+        if not self.completed():
+            return
+        else:
+            # TODO Mutations Logic
+            pass
+    
+    def variation(self):
+        if not self.completed():
+            return
+        else:
+            # TODO Variation Logic
+            pass
+
+
+
 # Main Function (Entry point)
 if __name__ == "__main__":
     br17 = initXML("br17.xml")
     print(br17)
+
+    test_client = Client(br17)
+    print(test_client)
 
     # Wer lust hat, kann das ja mal ausprobieren xD
     # a280 = initXML("a280.xml")
